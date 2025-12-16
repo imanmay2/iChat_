bdb889eb37ba420c33b23436d649d26ca89ef6a4
manmay2 <imanmay2@gmail.com>
2025-12-16 20:01:12 +0530

    let's start building the UI of the Auth page

--- client/src/auth/Login.tsx
+++ client/src/auth/Login.tsx
@@ -1,13 +1,9 @@
 
 
-
 const Login = () => {
   return (
-    <div className="main">
-        <div>B1</div>
-        <div>B2</div>
-        <div>B3</div>
-        <div>B4</div>
+    <div>
+      
     </div>
   )
 };
